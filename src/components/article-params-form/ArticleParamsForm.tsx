4a561b7da5ffa52b17c1abd0a247b276c40e89f3
Evgenii Ansel <ave1122@yandex.ru>
2025-03-13 22:37:43 +0300

    sidebar

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,105 @@
-import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
-
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { ArrowButton } from '../../components/arrow-button';
+import { Button } from '../../components/button';
+import { useState } from 'react';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Spacing } from '../spacing';
+import { Text } from '../../components/text';
+import { Select } from '../select';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+} from '../../../src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsFormProps {
+	setArticleState: (value: typeof defaultArticleState) => void;
+}
+
+export const ArticleParamsForm = ({
+	setArticleState,
+}: ArticleParamsFormProps) => {
+	const [open, setOpen] = useState(false);
+	const [settings, setSettings] = useState(defaultArticleState);
+
+	const handleApply = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setArticleState(settings);
+	};
+
+	const handleReset = () => {
+		setSettings(defaultArticleState);
+		setArticleState(defaultArticleState);
+	};
+
+	const updateSetting = (field: keyof typeof settings, value: any) => {
+		setSettings((prev) => ({ ...prev, [field]: value }));
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={open} setOpen={setOpen} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: open })}>
+				<form
+					className={styles.form}
+					onSubmit={handleApply}
+					onReset={handleReset}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Spacing size={50} />
+					<Select
+						title='Шрифт'
+						selected={settings.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => updateSetting('fontFamilyOption', option)}
+					/>
+					<Spacing size={50} />
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={settings.fontSizeOption}
+						onChange={(option) => updateSetting('fontSizeOption', option)}
+						title='Размер шрифта'
+					/>
+					<Spacing size={50} />
+
+					<Select
+						title='Цвет шрифта'
+						selected={settings.fontColor}
+						options={fontColors}
+						onChange={(option) => updateSetting('fontColor', option)}
+					/>
+					<Spacing size={50} />
+
+					<Separator />
+					<Spacing size={50} />
+
+					<Select
+						title='Цвет фона'
+						selected={settings.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => updateSetting('backgroundColor', option)}
+					/>
+					<Spacing size={50} />
+
+					<Select
+						title='Ширина контента'
+						selected={settings.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => updateSetting('contentWidth', option)}
+					/>
+					<Spacing size={50} />
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button title='Сбросить' type='clear' onClick={handleReset} />
+						<Button title='Применить' type='apply' htmlType='submit' />
 					</div>
 				</form>
 			</aside>
